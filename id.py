88a7d026f4b70d0014163a3d66b2b8d2c3beb823
Ethan_Hadley <ekhadley@gmail.com>
2024-01-18 18:21:40 -0600

    started on band idfcation

--- id.py
+++ id.py
@@ -9,28 +9,69 @@ elif system == "Linux":
 name = "0.png"
 path = os.path.join(idir, name)
 
-def endpoints():
-    return (400, 400), (700, 700)
+def endpoints(im: np.ndarray):
+    return (.48, .48), (.305, .76)
 
-def markends(im, points):
-    assert len(points) == 2, f"expected 2 end points, got {len(points)}"
-    p1, p2 = points
-    x1, y1 = p1
-    x2, y2 = p2
+def crop_to_ends(im: np.ndarray, points: tuple[float]):
+    end1, end2 = points
+    x1, y1, x2, y2 = *end1, *end2
+    px1, py1, px2, py2 = *relpix(im, x1, y1), *relpix(im, x2, y2)
+    if px1 > px2: px1, px2 = px2, px1
+    if py1 > py2: py1, py2 = py2, py1
+    return im.copy()[py1-50:py2+50, px1-50:px2+50]
 
-    cv2.circle(im, (x1, y1), 30, (0, 0, 255), 10)
-    cv2.circle(im, (x2, y2), 30, (0, 0, 255), 10)
-    return im
+def mark_ends(im: np.ndarray, ends: tuple[float]):
+    assert len(ends) == 2, f"expected 2 end points, got {len(ends)}"
+    end1, end2 = ends
+    x1, y1, x2, y2 = *end1, *end2
 
+    out = cv2.circle(im.copy(), relpix(im, x1, y1), 30, (0, 0, 255), 10)
+    out = cv2.circle(out, relpix(im, x2, y2), 30, (0, 0, 255), 10)
+    return out
+
+def normalize(im: np.ndarray, ends: tuple[float], crop=True):
+    end1, end2 = ends
+    x1, y1, x2, y2 = *end1, *end2
+    px1, py1, px2, py2 = *relpix(im, x1, y1), *relpix(im, x2, y2)
+    a = math.degrees(math.atan2(py2-py1, px2-px1))
+    a = abs(a)
+    #if a < 0: a += 360
+    out = rotate_image(im, -a, center=(px1, py1))
+    if crop:
+        dist = int(np.linalg.norm((px1-px2, py1-py2)))
+        out = out[py1-200:py1+200, px1:px1+dist]
+        #out = out[py1-20:py1+20, px1:px1+dist] # the slice used for color gradient examination
+    return out
+
+def gradient(im: np.ndarray, retim=True):
+    h, w, d = im.shape
+    my = h//2
+    s = im[my-30:my+30,:]
+    g = np.median(s, axis=0)
+    if retim: return g, s
+    return g
+
+import matplotlib .pyplot as plt
+
+fig, ax = plt.subplots()
+ax.set_prop_cycle(color=["blue", "green", "red"])
 if __name__ == "__main__":
-    print(f"{yellow}loaded image at {path}{endc}")
-    
     im = cv2.imread(path)
-    print(f"{lime}imge has type:{type(im)}{endc}")
-    print(f"{green}image has dimensions:{im.shape}{endc}")
+    im = cv2.GaussianBlur(im, (13,13), 0)
     
-    ends = endpoints()
-    markends(im, ends)
+    print(f"{yellow}loaded image at {path}{endc}")
+    print(f"{lime}image has type:{type(im)}{endc}")
+    print(f"{green}image has dimensions:{im.shape}{endc}")
 
-    imshow('im', im, .25)
+    ends = endpoints(im)
+    marked = mark_ends(im, ends)
+    rotated = normalize(im, ends, crop=True)
+    grad, slic = gradient(rotated)
+
+    ax.plot(grad)
+
+    imshow('im', marked, .25)
+    imshow('rotated', rotated, .25)
+    imshow('sliced', slic)
+    plt.show()
     cv2.waitKey(0)
--- utils.py
+++ utils.py
@@ -1,13 +1,31 @@
-import cv2, time, os, platform
+import cv2, time, os, platform, math
 import numpy as np
+from tqdm import trange
 
 
 def imscale(img, s):
-    assert not 0 in np.shape(img), "empty src image"
+    assert not 0 in img.shape, "empty src image"
     return cv2.resize(img, (round(len(img[0])*s), round(len(img)*s)), interpolation=cv2.INTER_NEAREST)
 
-def imshow(name, img, s):
+def imshow(name, img, s=1.0):
     cv2.imshow(name, imscale(img, s))
+
+def relpix(img, *args):
+    h, w, d = img.shape
+    if len(args) == 1 and isinstance(args[0], tuple(float)):
+        x, y = args[0]
+    elif len(args) == 2 and isinstance(args[0], float) and isinstance(args[1], float):
+        x, y = args
+    else: raise TypeError(f"expected 2 float args or tuple of 2 floats, got args: {args}")
+    return int(y*h), int(x*w)
+
+def rotate_image(image, degrees, center=None):
+    if center is None:
+        h, w, d = image.shape
+        center = (w//2, h//2)
+    mat = cv2.getRotationMatrix2D(center, angle=degrees, scale=1.0)
+    result = cv2.warpAffine(image, mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
+    return result
 
 purple = '\033[95m'
 blue = '\033[94m'
