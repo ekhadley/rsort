f4e35a69b1d4a40c2acbbb872c06d82c13ad3ade
ek <ekhadley@gmail.com>
2024-02-11 09:03:56 -0600

    asd

--- capture.py
+++ capture.py
@@ -5,7 +5,7 @@ pc2 = Picamera2()
 stillConf = pc2.create_still_configuration()
 pc2.start(config=stillConf)
 time.sleep(2)
-save = "//home//ek//Desktop//wgmn//rsort//testes"
+save = "//home//ek//Desktop//wgmn//rsort//ims"
 c = 0
 
 while 1:
@@ -16,9 +16,9 @@ while 1:
     ms = np.shape(marked)
     marked = cv2.circle(marked, (ms[1]//2, ms[0]//2), 5, (50, 0, 250), 2)
 
-    cv2.imshow('im', marked)
+    imshow('im', marked, 0.25)
     if cv2.waitKey(1) & 0xFF == ord('q'):
-        cv2.imwrite(f"{save}{c}.png", im)
+        cv2.imwrite(f"{save}{c}a.png", im)
         c += 1
         print(f"saved: {c}")
         time.sleep(1)
