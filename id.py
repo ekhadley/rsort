f604452dd176d1fae9437dfccf93ac957d1c8834
eekay <ekhadley@gmail.com>
2024-02-11 22:43:14 -0600

    refactoring ugh

--- id.py
+++ id.py
@@ -1,52 +1,11 @@
 from utils import *
 
-def endpoints(im: np.ndarray):
-    #return (.475, .489), (.315, .747) # im0
-    return (.41, .4), (.25, .67) # im1
-    #return (.34, 0.985), (.15, .758) # im2
+def band_colors(strp: np.ndarray):
+    base = strp - np.array([134.06326531, 99, 45])
+    avgs = np.mean(base, axis=0)
+    ints = np.mean(np.sqrt(np.sum(np.square(base), axis=2)), axis=0)
 
-def crop_to_ends(im: np.ndarray, points: tuple):
-    end1, end2 = points
-    x1, y1, x2, y2 = *end1, *end2
-    px1, py1, px2, py2 = *relpix(im, x1, y1), *relpix(im, x2, y2)
-    if px1 > px2: px1, px2 = px2, px1
-    if py1 > py2: py1, py2 = py2, py1
-    return im.copy()[py1-50:py2+50, px1-50:px2+50]
-
-def mark_ends(im: np.ndarray, ends: tuple):
-    assert len(ends) == 2, f"expected 2 end points, got {len(ends)}"
-    end1, end2 = ends
-    x1, y1, x2, y2 = *end1, *end2
-
-    out = cv2.circle(im.copy(), relpix(im, x1, y1), 30, (0, 0, 255), 10)
-    out = cv2.circle(out, relpix(im, x2, y2), 30, (0, 0, 255), 10)
-    return out
-
-def isolate(im: np.ndarray, ends: tuple):
-    end1, end2 = ends
-    x1, y1, x2, y2 = *end1, *end2
-    px1, py1, px2, py2 = *relpix(im, x1, y1), *relpix(im, x2, y2)
-    a = math.degrees(math.atan2(py2-py1, px2-px1))
-    a = abs(a)
-    #if a < 0: a += 360
-    out = rotate_image(im, -a, center=(px1, py1))
-    dist = int(np.linalg.norm((px1-px2, py1-py2)))
-    #out = out[py1-30:py1+30, px1+50:px1+dist-50].astype(np.float32)
-    out = out[py1-30:py1+30, px1+50:px1+dist-50]
-    return out # the slice used for color gradient examination
-
-def gradient(strp_: np.ndarray):
-    strp = strp_.copy().astype(np.float32)
-    base = np.array([134.06326531, 99, 45]) # subtract the bluey base resistor color
-    strp -= base
-    #strp -= np.mean(strp, axis=(0,1))
-    
-    avgs = np.mean(strp_, axis=(0,)) # average color along columns of rgb values
-    #sums = np.sum(strp, axis=(0,)) # sum of rgbs along columns
-    #mag = np.mean(strp, axis=(0,2)) # average color value (average of averages of rgb) along columns
-    ints = np.mean(np.sqrt(np.sum(np.square(strp), axis=2)), axis=(0))
-
-    bands, _ = scipy.signal.find_peaks(ints,
+    bandpos, _ = scipy.signal.find_peaks(ints,
                                     height=2,
                                     threshold=None,
                                     distance=50,
@@ -55,54 +14,54 @@ def gradient(strp_: np.ndarray):
                                     width=None,
                                     rel_height=0.5,
                                     plateau_size=None)
-    colors = [np.mean(avgs[band-10:band+10], axis=(0)) for band in bands]
-    return strp.astype(np.uint8), avgs, ints, bands, colors
+    bandcolors = [np.mean(avgs[band-10:band+10], axis=(0)) for band in bandpos]
+    return base, avgs, ints, bandpos, bandcolors
 
-def row_cluster(strp, K=1):
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-    _, label, center = cv2.kmeans(np.float32(strp), K, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
-    center = np.uint8(center)
-    res = center[label.flatten()]
-    return res.reshape((strp.shape)).astype(strp.dtype)
+def endpoints(im: np.ndarray):
+    center = [950, 1231], [1452, 816] # im0
+    #center = [777, 1-62], [1302, 648] # im1
+    #center = [1914, 881], [1473, 388] # im2
 
-def col_cluster(strp_: np.ndarray, K=15):
-    strp = strp_.swapaxes(0, 1)
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-    _, label, center = cv2.kmeans(np.float32(strp), K, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
-    center = np.uint8(center)
-    res = center[label.flatten()]
-    return res.reshape(strp.shape).swapaxes(0, 1)
+    hsl = cv2.cvtColor(im, cv2.COLOR_BGR2HLS)
+    mask = (hsl[:,:,2] > 50).astype(np.uint8)
+    masked = im*np.expand_dims(mask, -1)
+    stats = cv2.connectedComponentsWithStats(mask)
+    print(bold, blue, len(stats), endc)
+    print(bold, blue, len(stats[0]), endc)
+    print(bold, green, stats[0][4], endc)
+    for i in stats[:,cv2.CC_STAT_AREA]:
+        print(bold, i, endc)
+
+    
+    return center, hsl, mask, masked
 
 fig, ax = plt.subplots()
 ax.set_prop_cycle(color=["blue", "green", "red", "black"])
 if __name__ == "__main__":
-    im = load_test_im( "1.png")
+    im = load_test_im("0.png")
+    print("jkbhsdfbhjvsdfvbhj")
     #print(f"{green}image has dimensions:{im.shape}{endc}")
     #im = cv2.GaussianBlur(im, (13,13), 0)
-    im = cv2.bilateralFilter(im, 25, 75, 75) 
+    im = cv2.bilateralFilter(im, 35, 75, 75) 
 
-    ends = endpoints(im)
+    ends, hsl, mask, masked = endpoints(im)
     marked = mark_ends(im, ends)
     cropped = isolate(im, ends)
-    strp, avgs, intensity, bands, colors = gradient(cropped)
+    strp, avgs, intensity, bandpos, bandcolors = band_colors(cropped)
     
-    #[print(f"{blue}{c.rgb}: {c.proportion:.3f}{endc}") for c in cg.extract(Image.fromarray(np.flip(strp, axis=-1)), 5)]
+    #[print(f"{blue}{c.rgb}: {c.proportion:.3f}{endc}") for c in cg.extract(Image.fromarray(np.flip(im, axis=-1)), 5)]
     
     ax.plot(avgs)
     ax.plot(intensity)
-    ax.plot(bands, intensity[bands], "o", ms=10, color="orange" )
-    
-    for band in bands:
-        cropped = cv2.rectangle(cropped, (band-2, 0), (band+2, len(cropped)), color=(50,0,250), thickness=-1)
-
-    ret, binary = cv2.threshold(cv2.cvtColor(im, cv2.COLOR_BGR2GRAY), 180, 255, cv2.THRESH_BINARY_INV)
-    center = [ round(np.average(indices)) for indices in np.where(binary >= 255) ] 
-    cv2.circle(marked, center, 50, (0, 250, 0), 10)
+    ax.plot(bandpos, intensity[bandpos], "o", ms=10, color="orange" )
     
     imshow('im', marked, .25)
-    imshow('cropped', cropped)
-    imshow('processed', strp)
+    #imshow('cropped', cropped)
+    imshow('processed', mark_bands(strp, bandpos))
     #imshow('bin', binary, 0.25)
-    imshow('vis', visualize_bands(colors), 0.25)
+    imshow('mask', mask*255, 0.25)
+    imshow('masked', masked, 0.25)
+    #imshow('vis', visualize_bands(colors), 0.25)
     plt.show()
     cv2.waitKey(0)
+    cv2.destroyAllWindows()
--- utils.py
+++ utils.py
@@ -7,6 +7,40 @@ import matplotlib
 import matplotlib.pyplot as plt
 import scipy
 
+#gry = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+#ret, binary = cv2.threshold(gry, 160, 255, cv2.THRESH_BINARY_INV)
+#center = [round(np.average(indices)) for indices in np.where(binary >= 255)] 
+#cv2.circle(marked, center, 50, (0, 250, 0), 10)
+
+#ret, binary = cv2.threshold(cv2.cvtColor(im, cv2.COLOR_BGR2GRAY), 180, 255, cv2.THRESH_BINARY_INV)
+
+def mark_ends(im: np.ndarray, ends: tuple):
+    assert len(ends) == 2, f"expected 2 end points, got {len(ends)}"
+    end1, end2 = ends
+    x1, y1, x2, y2 = *end1, *end2
+    out = cv2.circle(im.copy(), (x1, y1), 30, (0, 0, 255), 10)
+    out = cv2.circle(out,  (x2, y2), 30, (0, 0, 255), 10)
+    return out
+
+def crop_to_ends(im: np.ndarray, points: tuple):
+    end1, end2 = points
+    x1, y1, x2, y2 = *end1, *end2
+    if x1 > x2: x1, x2 = x2, x1
+    if y1 > y2: y1, y2 = y2, y1
+    return im.copy()[y1-50:y2+50, x1-50:x2+50]
+
+def isolate(im: np.ndarray, ends: tuple):
+    end1, end2 = ends
+    x1, y1, x2, y2 = *end1, *end2
+    a = math.degrees(math.atan2(y2-y1, x2-x1))
+    a = abs(a)
+    #if a < 0: a += 360
+    out = rotate_image(im, -a, center=(x1, y1))
+    dist = int(np.linalg.norm((x1-x2, y1-y2)))
+    #out = out[y1-30:y1+30, x1+50:x1+dist-50].astype(np.float32)
+    out = out[y1-30:y1+30, x1+50:x1+dist-50]
+    return out # the slice used for color gradient examination
+
 def visualize_bands(colors, scale=5.0):
     s = int(100*scale)
     out = np.zeros((s, s*len(colors), 3), dtype=np.uint8)
@@ -14,6 +48,13 @@ def visualize_bands(colors, scale=5.0):
         col = tuple([int(c) for c in color])
         out = cv2.rectangle(out, (s*i, 0), (s*(i+1), s), color=col, thickness=-1)
     return out
+
+def mark_bands(strp, bandpos):
+    out = np.array(strp, copy=True)
+    for band in bandpos:
+        out = cv2.rectangle(out, (band-2, 0), (band+2, strp.shape[0]), (100, 10, 250), -1)
+    return out
+
 
 def load_test_im(name):
     system = platform.system()
@@ -31,7 +72,7 @@ def imshow(name, img, s=1.0, wait=False):
     cv2.imshow(name, imscale(img, s))
     if wait: cv2.waitKey(0)
 
-def relpix(img, *args):
+def _relpix(img, *args):
     h, w, _ = img.shape
     if len(args) == 1 and isinstance(args[0], tuple):
         x, y = args[0]
@@ -47,6 +88,21 @@ def rotate_image(image, degrees, center=None):
     mat = cv2.getRotationMatrix2D(center, angle=degrees, scale=1.0)
     result = cv2.warpAffine(image, mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
     return result
+
+def row_cluster(strp, K=1):
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    _, label, center = cv2.kmeans(np.float32(strp), K, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+    center = np.uint8(center)
+    res = center[label.flatten()]
+    return res.reshape((strp.shape)).astype(strp.dtype)
+
+def col_cluster(strp_: np.ndarray, K=15):
+    strp = strp_.swapaxes(0, 1)
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    _, label, center = cv2.kmeans(np.float32(strp), K, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+    center = np.uint8(center)
+    res = center[label.flatten()]
+    return res.reshape(strp.shape).swapaxes(0, 1)
 
 purple = '\033[95m'
 blue = '\033[94m'
